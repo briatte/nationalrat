d2350bd4b03d69a9813ac1ce672a66f6bc85abdb
François <briatte@gmail.com>
2015-08-29 12:15:26 +0200

    fix %e% indexation issue

--- comm.r
+++ comm.r
@@ -75,12 +75,12 @@ for (i in unique(comm$legislature)) {
       sum(e$committee > 1), "> 1\n")
 
   nn = network(e[, 1:2], directed = FALSE)
-  nn %e% "committee" = e$committee
+  set.edge.attribute(nn, "committee", e$committee)
   
   print(table(nn %e% "committee", exclude = NULL))
   stopifnot(!is.na(nn %e% "committee"))
 
-  n %e% "committee" = e$committee
+  set.edge.attribute(n, "committee", e$committee)
   assign(paste0("net_at", years[ as.character(i) ]), n)
   
   nn %n% "committees" = as.table(rowSums(M))
